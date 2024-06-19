aedd2ec76447a3f64954c4c7e8dda2cc1aac021f
Amankapse <90203379+Amankapse@users.noreply.github.com>
2024-06-19 17:10:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    const message = document.getElementById('message');
+    const surprise = document.getElementById('surprise');
+    
+    message.classList.toggle('hidden');
+    surprise.classList.toggle('hidden');
+}
